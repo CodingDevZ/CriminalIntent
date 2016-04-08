5407fd422a368cfe05a129b7ea0296aedd878962
CodingDevZ <sharpp999@gmail.com>
2016-04-08 19:37:12 +0200

    Criminal Intent app

---
+++ app/src/main/java/com/example/tomy_/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,36 @@
+package com.example.tomy_.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.example.tomy_.criminalintent.Crime;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by tomy- on 1/9/2016.
+ */
+public class CrimeCursorWrapper extends CursorWrapper {
+
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SOLVED));
+        String suspect = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SUSPECT));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+        crime.setSuspect(suspect);
+
+        return crime;
+
+    }
+}
---
+++ app/src/main/java/com/example/tomy_/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.example.tomy_.criminalintent.database;
+
+/**
+ * Created by tomy- on 1/9/2016.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String SUSPECT = "suspect";
+        }
+    }
+}
